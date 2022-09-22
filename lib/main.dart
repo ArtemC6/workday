955da802fb8eddf15684d1d666ee7bb2d3bbf85c
ArtemC6 <and.artem.sedov@yandex.ru>
2022-09-22 12:42:03 +0600

    Commit Version - 29 Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// @dart=2.9
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +28,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
